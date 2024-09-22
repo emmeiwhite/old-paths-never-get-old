9f07f362e9e3138b69eb2a04819661d5a760b913
emmeiwhite <emmeiwhite@gmail.com>
2024-09-22 13:01:04 +0530

    camel-case for HTML attributes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import ReactDOM from 'react-dom/client'
 function Greeting() {
   return (
     <>
-      <h1>hello world!</h1>
+      <h1>Hello World!</h1>
+      <p>Enough talk, let's act</p>
+      <p>It's never too late, just believe in yourself</p>
+
+      {/* Camel case for HTML Properties */}
+      <div
+        tabIndex={1}
+        className="camel-case"
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          readOnly={true}
+          id="name"
+        />
+      </div>
     </>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
 root.render(<Greeting />)
