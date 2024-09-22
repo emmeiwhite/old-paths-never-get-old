a0920c34af78d7f916565f112e6a3b28afda0015
emmeiwhite <emmeiwhite@gmail.com>
2024-09-22 12:57:23 +0530

    React Fragment: let's us group elements without adding extra nodes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function Greeting() {
-  return <h1>My First Component</h1>
+  return (
+    <>
+      <h1>hello world!</h1>
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
