9c209bf6512d9aa3642c80b7805c99248c061705
emmeiwhite <emmeiwhite@gmail.com>
2024-09-24 10:02:41 +0530

    props (kind of parameters and arguments) for reusablility of components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import './index.css'
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        image={'./images/book-1.jpg'}
+        author="JAMAL T. MILLWOOD"
+        title="KIM’S LOST WORDS: A journey for justice, from the other side"
+      />
       <Book />
     </section>
   )
@@ -13,22 +17,21 @@ function BookList() {
 
 // Writing JS within JSX: We use curly brackets and use expressions within. Remember: An expression must return a value
 
-let title = 'KIM’S LOST WORDS: A journey for justice, from the other side'
-function Book() {
-  let author = 'JAMAL T. MILLWOOD'
+// Every component in React, has a props object. First steps towards making a component re-usable & dding dynamism to a component
+function Book(props) {
   return (
     <article className="book">
       <img
-        src="./images/book-1.jpg"
+        src={props.image}
         className="author-img"
-        alt="book"
+        alt={props.title}
       />
-      <h2 className="title">{title}</h2>
+      <h2 className="title">{props.title}</h2>
       <h4
         className="author"
         style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}
       >
-        <strong>Author:</strong> {author}
+        <strong>Author:</strong> {props.author}
       </h4>
     </article>
   )
